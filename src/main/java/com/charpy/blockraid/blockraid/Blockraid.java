3f6cab83b69638c8eecdb001c5c1126c6cb2635e
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-02-29 09:57:33 +0100

    Update Blockraid.java

--- src/main/java/com/charpy/blockraid/blockraid/Blockraid.java
+++ src/main/java/com/charpy/blockraid/blockraid/Blockraid.java
@@ -2,6 +2,8 @@ package com.charpy.blockraid.blockraid;
 
 import com.charpy.blockraid.blockraid.proxy.CommonProxy;
 import com.charpy.blockraid.blockraid.util.Reference;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -14,6 +16,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import javax.security.auth.login.LoginException;
 
 @Mod(modid =  Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
 
@@ -33,7 +37,13 @@ public class Blockraid {
 
     @Mod.EventHandler
     public static void init(FMLInitializationEvent event) {
-
+        try {
+            JDABuilder.createDefault("YOUR_DISCORD_BOT_TOKEN")
+                    .setActivity(Activity.playing("Minecraft"))
+                    .build();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
     }
 
     @Mod.EventHandler
