d2c7eaec54b971e3e6f45fe55cb0e0b14494a23b
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-03-10 21:47:40 +0100

    Update Blockraid.java

--- src/main/java/com/charpy/blockraid/blockraid/Blockraid.java
+++ src/main/java/com/charpy/blockraid/blockraid/Blockraid.java
@@ -2,8 +2,6 @@ package com.charpy.blockraid.blockraid;
 
 import com.charpy.blockraid.blockraid.proxy.CommonProxy;
 import com.charpy.blockraid.blockraid.util.Reference;
-import net.arikia.dev.drpc.DiscordRPC;
-import net.arikia.dev.drpc.DiscordRichPresence;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -29,16 +27,8 @@ public class Blockraid {
 
     @Mod.EventHandler
     public static void init(FMLInitializationEvent event) {
-        DiscordRPC.discordInitialize("1193252034548355162", null, false);
-        DiscordRichPresence presence = new DiscordRichPresence();
-        presence.startTimestamp = System.currentTimeMillis() / 1000;
-        presence.details = "Making RPC";
-        presence.state = "Working on :)";
-        presence.largeImageKey = "ww2";
-        presence.largeImageText = "ez";
-        DiscordRPC.discordUpdatePresence(presence);
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> DiscordRPC.discordShutdown()));
     }
+    
     @Mod.EventHandler
     public static void PostInit(FMLPostInitializationEvent event) {
 
